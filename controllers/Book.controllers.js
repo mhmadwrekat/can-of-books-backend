12b3f70a772b4aec6a857cc54f89e291e8c869d0
mhmad wrekat <mhmmadwreekkat@gmail.com>
2021-09-19 17:16:38 +0300

    finishh

--- controllers/Book.controllers.js
+++ controllers/Book.controllers.js
@@ -3,7 +3,7 @@ const { bookModel } = require('../models/Book.models');
 
 let bookController = (req, res) => {
     bookModel.find().then(data => {
-        res.json(data);
+        res.status(200).json(data);
     })
 }
 module.exports = bookController;
--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const MONGO_SERVER = process.env.MONGO_SERVER;
 mongoose.connect(`${MONGO_SERVER}/Book`, { useNewUrlParser: true, useUnifiedTopology: true });
 const bookController = require('./controllers/Book.controllers');
 
+//❤️Welcome Message❤️
+app.get('/', (req, res) => {
+    res.status(200).json({ "❤️": "Hello from The Back-End !" })
+  })
 //❤️❤️❤️❤️❤️
 app.get('/DataBooks', bookController);
 
